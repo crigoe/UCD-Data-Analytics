ba74362e527ec78f33c9a222ff4fe22eec496bc8
Christian <christiangoergen@gmail.com>
2021-09-11 16:42:01 +0200

    "Introduction to Python" @ Datacamp

--- Datacamp_Introduction_Baseball.py
+++ Datacamp_Introduction_Baseball.py
@@ -8,11 +8,17 @@ data = pd.read_csv ('baseball.csv')
 
 height_in = data['Height'].tolist()
 weight_in = data['Weight'].tolist()
+age_in = data['Age'].tolist()
+
 
 # Create a numpy arrays
 np_height_in = np.array(height_in)
 np_weight_in = np.array(weight_in)
+np_age_in = np.array(age_in)
 
+
+baseball = [np_height_in, np_weight_in]
+np_baseball = np.array(baseball)
 
 # Convert inch to m and lb to kg
 np_height_m = np_height_in * 0.0254
@@ -20,5 +26,18 @@ np_weight_kg = np_weight_in * 0.453592
 
 # Calculate BMI
 bmi = np_weight_kg / np_height_m ** 2
-# Print BMI
-print (bmi)
+
+# Define light
+light = bmi < 21
+
+# Print out the weight at index 50
+# print (np_weight_kg[50])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+# print (np_height_m[100:111])
+
+# print (np_baseball)
+
+# Print out the shape of np_baseball
+# print(np_baseball.shape)
+
