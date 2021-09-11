336388fd58b6551733084fde17f0a94e7684636e
Christian <christiangoergen@gmail.com>
2021-09-11 10:36:07 +0200

    Update Project

--- My first Python file.py
+++ My first Python file.py
@@ -1,6 +1,24 @@
+# Import numpy
 import numpy as np
-A = "Test"
-print (A)
-# This is my first Python project
-B = np.array([1,2,3])
-print (B)
+import pandas as pd
+
+# Get list np_heigh_in from baseball.csv
+
+data = pd.read_csv ('baseball.csv')
+
+height_in = data['Height'].tolist()
+weight_in = data['Weight'].tolist()
+
+# Create a numpy arrays
+np_height_in = np.array(height_in)
+np_weight_in = np.array(weight_in)
+
+
+# Convert inch to m and lb to kg
+np_height_m = np_height_in * 0.0254
+np_weight_kg = np_weight_in * 0.453592
+
+# Calculate BMI
+bmi = np_weight_kg / np_height_m ** 2
+# Print BMI
+print (bmi)
