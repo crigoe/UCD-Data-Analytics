a9c01d4e57e76d5a1a1760563ae45d93f9bd7890
Christian <christiangoergen@gmail.com>
2021-09-28 16:20:07 +0200

    Regular Expressions

---
+++ Data Cleaning - AirBnB - NYC.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+airbnb_data = pd.read_csv('AB_NYC_2019.csv')
+print (airbnb_data[0:0])
+print (airbnb_data.shape)
+
+missing_values_count = airbnb_data.isnull().sum()
+print(missing_values_count[:])
+
+# DataFrame
+show_me = pd.DataFrame(airbnb_data)
+print (show_me['minimum_nights'])
--- Regular Expressions.py
+++ Regular Expressions.py
@@ -1,2 +1,38 @@
 import re
-re.findall("#excited", "I am so #excited to present this certificate")
+
+# Look for string in a string
+#output = re.findall('excited','i am so excited')
+
+# Split a string
+# output = re.split ("!", "Split this sentence ! when you find the sign and store it in a list")
+
+# Replace one or many values
+# output = re.sub ('red','blue','I have a red car')
+
+# \d for Digits
+# output = re.findall("user\d","the winners are user1, user2, user3, userb")
+
+# \D for NonDigits
+# output = re.findall("user\D","the winners are user1, user2, user3, userb")
+
+# \w for Word (Digits + NonDigits)
+# output = re.findall("user\w","the winners are user1, user2, user3, userb")
+
+# \W for Word (Non-Word)
+# output = re.findall("user\W","the winners are user1, user2, user3, userb, user$")
+
+# \s for White Space
+# output = re.findall("Data Science","This is Data Science")
+
+# \S for Non-Whitespace
+# output = re.sub("ice\Scream","ice cream","I like ice-cream")
+
+# zero times or 1
+# output = re.findall("colou?r","This is my color. Loce this colour")
+
+#output = re.findall("\+\d{3}-\d{5}\s\d{4}", "+353-98765 4321")
+
+data = "ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef, ejhefjhefjhfe, johnsmith12@gmail.com, ejfefhjfehjef"
+output = re.findall("\w+@\w+\Wcom", data)
+
+print (output)
